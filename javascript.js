98bec45bf337e0fab0aeb47af848435d3f99112f
Fellryn <163130057+Fellryn@users.noreply.github.com>
2025-10-24 19:17:01 +1000

    Table read and delete buttons added

--- javascript.js
+++ javascript.js
@@ -17,22 +17,17 @@ addBookToLibrary(book1);
 const book2 = new Book("Three Body Problem", "Some Guy", false)
 addBookToLibrary(book2);
 
-const book3 = new Book("Three Body Problem", "Some Guy", false)
-addBookToLibrary(book3);
-
-const book4 = new Book("Three Body Problem", "Some Guy", false)
-addBookToLibrary(book3);
-
 
 const table = document.querySelector("#library-table");
 const tableBody = table.querySelector("tbody");
 
 for (let book of library) {
-    const entry = `<tr>
+    const entry = `<tr data-id="${book.id}">
                     <td>${book.title}</td>
                     <td>${book.author}</td>
                     <td>${book.read}</td>
                     <td>${book.id}</td>
+                    <td><button class="btn-read">Read</button><button class="btn-delete">Delete</button></td>
                     </tr>`;
     tableBody.innerHTML += (entry);
 }
