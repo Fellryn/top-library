60e13142962f9b505d203b9dd8ebaceab090b906
Fellryn <163130057+Fellryn@users.noreply.github.com>
2025-10-28 12:28:57 +1000

    Add book class

--- javascript.js
+++ javascript.js
@@ -17,12 +17,21 @@ form.addEventListener('submit', (e) => {
     form.reset();
 });
 
-function Book(title, author, read) {
-    this.title = title;
-    this.author = author;
-    this.read = read;
-    this.id = crypto.randomUUID();
+class Book {
+    constructor(title, author, read) {
+        this.title = title;
+        this.author = author;
+        this.read = read;
+        this.id = crypto.randomUUID();
+    }
 }
+
+// function Book(title, author, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.read = read;
+//     this.id = crypto.randomUUID();
+// }
 
 function addBookToLibrary(book) {
     library.push(book);        
